ec67404f2dabba180e89486920ee660bda207eec
Elisey <shemetov.elisey@gmail.com>
2020-09-28 14:08:33 +0700

    add university class

--- CSharp_lab2/IUniversity.cs
+++ CSharp_lab2/IUniversity.cs
@@ -1,7 +1,21 @@
-﻿using System;
+﻿using System.Collections.Generic;
+
 namespace CSharp_lab2
 {
-	public interface IUniversity
-	{
-	}
+   interface IUniversity
+   {
+      IEnumerable<IPerson> Persons { get; }  // Сортировка по принципу ПЗ1
+      IEnumerable<Student> Students { get; }  // Сортировка по принципу ПЗ1
+      IEnumerable<Teacher> Teachers { get; }  // Сортировка по принципу ПЗ1
+
+      void Add(IPerson person);
+      void Remove(IPerson person);
+
+      IEnumerable<IPerson> FindByLastName(string lastName);
+
+      // Для нечетных вариантов (нечетный номер бригады): 
+      // Выдать всех студентов, чей средний балл выше заданного.
+      // Отсортировать по среднему баллу.
+      IEnumerable<Student> FindByAvrPoint(float avrPoint);
+   }
 }
--- CSharp_lab2/University.cs
+++ CSharp_lab2/University.cs
@@ -1,10 +1,38 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace CSharp_lab2
 {
-	public class University
+	public class University : IUniversity
 	{
 		public University()
 		{
 		}
-	}
+
+      public IEnumerable<IPerson> Persons => throw new NotImplementedException();
+
+      public IEnumerable<Student> Students => throw new NotImplementedException();
+
+      public IEnumerable<Teacher> Teachers => throw new NotImplementedException();
+
+      public void Add(IPerson person)
+      {
+         throw new NotImplementedException();
+      }
+
+      public IEnumerable<Student> FindByAvrPoint(float avrPoint)
+      {
+         throw new NotImplementedException();
+      }
+
+      public IEnumerable<IPerson> FindByLastName(string lastName)
+      {
+         throw new NotImplementedException();
+      }
+
+      public void Remove(IPerson person)
+      {
+         throw new NotImplementedException();
+      }
+   }
 }
