ad89d760d5718b38c615541835178e911116be34
Elisey <shemetov.elisey@gmail.com>
2020-12-09 01:18:48 +0700

    fix console interface

--- CSharp_lab2/Program.cs
+++ CSharp_lab2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace CSharp_lab2
@@ -11,8 +12,9 @@ namespace CSharp_lab2
             System.Console.InputEncoding = System.Text.Encoding.Unicode;
 
             var university = new University();
+            var flag = true;
 
-            while (true)
+            while (flag)
             {
                 Console.WriteLine("Введите необходимую клавишу " +
                                   "чтобы исполнить команду");
@@ -27,46 +29,46 @@ namespace CSharp_lab2
 
                 int input = int.Parse(Console.ReadLine());
 
-                if (input == 0)
-                {
-                    break;
-                }
                 switch (input)
                 {
                     case 1:
-                        foreach (var student in university.Students)
-                        {
-                            Console.WriteLine(student);
-                        }
+                        if (university.Students.Any())
+                            foreach (var student in university.Students)
+                                Console.WriteLine(student);
+                        else
+                            Console.WriteLine("Список студентов пуст");
                         break;
+
                     case 2:
-                        foreach (var teacher in university.Teachers)
-                        {
-                            Console.WriteLine(teacher);
-                        }
+                        if (university.Teachers.Any())
+                            foreach (var teacher in university.Teachers)
+                                Console.WriteLine(teacher);
+                        else
+                            Console.WriteLine("Список препадователей пуст");
                         break;
+
                     case 3:
-                        foreach (var person in university.Persons)
-                        {
-                            Console.WriteLine(person);
-                        }
+                        if (university.Persons.Any())
+                            foreach (var person in university.Persons)
+                                Console.WriteLine(person);
+                        else
+                            Console.WriteLine("Список людей пуст");
                         break;
+
                     case 4:
                         Console.WriteLine("Введите фамилию: ");
                         var lastName = Console.ReadLine();
                         foreach (var person in university.FindByLastName(lastName))
-                        {
                             Console.WriteLine(person.ToString());
-                        }
                         break;
+
                     case 5:
                         Console.WriteLine("Введите средний балл: ");
                         var avgPoint = float.Parse(Console.ReadLine());
                         foreach (var student in university.FindByAvgPoint(avgPoint))
-                        {
                             Console.WriteLine(student.ToString());
-                        }
                         break;
+
                     case 6:
                         Console.WriteLine("Введите фамилию: ");
                         lastName = Console.ReadLine();
@@ -75,45 +77,49 @@ namespace CSharp_lab2
 
                         Console.WriteLine("Это студент? 1 - если да, 2 - если нет");
                         var isStudentInput = Console.ReadLine();
+
                         IOrderedEnumerable<IPerson> personList;
                         if (isStudentInput == "1")
-                        {
                             personList = from student in university.Students
                                          where student.Name == name
                                          && student.LastName == lastName
                                          orderby student.LastName
                                          select student;
-                        }
                         else
-                        {
                             personList = from teacher in university.Teachers
                                          where teacher.Name == name
                                          && teacher.LastName == lastName
                                          orderby teacher.LastName
                                          select teacher;
-                        }
+
                         foreach (var person in personList)
-                        {
                             university.Remove(person);
-                        }
                         break;
+
                     case 7:
                         Console.WriteLine("Это студент? 1 - если да, 2 - если нет");
                         isStudentInput = Console.ReadLine();
+                        
+                        Console.WriteLine("Введите фамилию: ");
+                        lastName = Console.ReadLine();
+
+                        Console.WriteLine("Введите имя: ");
+                        name = Console.ReadLine();
+
+                        Console.WriteLine("Введите отчество: ");
+                        var patronymic = Console.ReadLine();
+
+                        Console.WriteLine("Введите дату рождения: ");
+                        var date = DateTime.Parse(Console.ReadLine());
+                        
                         if (isStudentInput == "1")
                         {
-                            Console.WriteLine("Введите фамилию: ");
-                            lastName = Console.ReadLine();
-                            Console.WriteLine("Введите имя: ");
-                            name = Console.ReadLine();
-                            Console.WriteLine("Введите отчество: ");
-                            var patronymic = Console.ReadLine();
-                            Console.WriteLine("Введите дату рождения: ");
-                            var date = DateTime.Parse(Console.ReadLine());
                             Console.WriteLine("Введите курс: ");
                             var course = int.Parse(Console.ReadLine());
+
                             Console.WriteLine("Введите группу: ");
                             var group = int.Parse(Console.ReadLine());
+                            
                             Console.WriteLine("Введите средний балл: ");
                             avgPoint = float.Parse(Console.ReadLine());
 
@@ -122,18 +128,12 @@ namespace CSharp_lab2
                         }
 						else
 						{
-                            Console.WriteLine("Введите фамилию: ");
-                            lastName = Console.ReadLine();
-                            Console.WriteLine("Введите имя: ");
-                            name = Console.ReadLine();
-                            Console.WriteLine("Введите отчество: ");
-                            var patronymic = Console.ReadLine();
-                            Console.WriteLine("Введите дату рождения: ");
-                            var date = DateTime.Parse(Console.ReadLine());
                             Console.WriteLine("Введите факультет: ");
                             var faculty = Console.ReadLine();
-                            Console.WriteLine("Введите опыт работы: ");
+                            
+                            Console.WriteLine("Введите опыт работ: ");
                             var experience = int.Parse(Console.ReadLine());
+                            
                             Console.WriteLine("Введите должность: ");
                             var position = (Positions)Enum.Parse(typeof(Positions),
                                                                  Console.ReadLine());
@@ -143,7 +143,9 @@ namespace CSharp_lab2
                                                        position));
                         }
                         break;
+
                     default:
+                        flag = false;
                         break;
                 }
             }
